a0f97f9391aa6a669c58f1a8d26cbfd690dc00f4
donny <donny@tams.informatik.uni-hamburg.de>
2019-03-14 22:17:27 +0100

    node to send velocities

---
+++ scripts/behavior.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+
+cmd_vel = None
+move_base = None
+
+cmd_twist = Twist()
+
+if __name__ == '__main__':
+    rospy.init_node('behavior')
+    cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=1)
+
+    cmd_twist.linear.x = 0.2
+    cmd_twist.angular.z = 0.7
+
+    r = rospy.Rate(10)  
+
+    while not rospy.is_shutdown()      
+        pub.publish(cmd_twist)
+        r.sleep()
