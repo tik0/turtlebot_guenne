d2c6add99c4f466a9467afb7221ea132b5352c39
donny <donny@tams.informatik.uni-hamburg.de>
2019-03-18 00:59:23 +0100

    donny sunday evening

--- scripts/behavior.py
+++ scripts/behavior.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
+import numpy as np
 import rospy
 from std_msgs.msg import Header
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
-
+from tf.transformations import quaternion_from_euler
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
@@ -20,7 +21,19 @@ def scan_callback(msg):
 
 def scored_callback(msg):
     # print "I scored"
-    pass
+    print(msg)
+    goal = MoveBaseGoal()
+    goal.target_pose.header.frame_id = 'map'
+    goal.target_pose.pose.position.x = 1.9
+    goal.target_pose.pose.position.y = 1.3
+    orientation = quaternion_from_euler(0, 0, -3.14/2)
+    goal.target_pose.pose.orientation.x=orientation[0]
+    goal.target_pose.pose.orientation.y=orientation[1]
+    goal.target_pose.pose.orientation.z=orientation[2]
+    goal.target_pose.pose.orientation.w=orientation[3]
+    move_base.send_goal(goal)
+    move_base.wait_for_result()
+
 
 if __name__ == '__main__':
     # initialization
@@ -38,18 +51,26 @@ if __name__ == '__main__':
     cmd_twist.linear.x = 0.2
     cmd_twist.angular.z = 0.7
 
-    #r = rospy.Rate(10)
-    #while not rospy.is_shutdown():
+    r = rospy.Rate(90)
+    x_vect = [5.9,  9.92,  1.9,  -1.42  , 7.1,  4.4,  6.03,  6.45,  4.83,  2.86]
+    y_vect = [-5.4, -4.76, -5.2, -0.68  , -2.5, -4.4, -2.01, -2.50, -0.83,  -1.58]
+    i=0
+    while not rospy.is_shutdown():
     #    cmd_vel.publish(cmd_twist)
     #    r.sleep()
 
 
     # run towards a point on the map
-    goal = MoveBaseGoal()
-    goal.target_pose.header.frame_id = 'map'
-    goal.target_pose.pose.position.x = 0.0
-    goal.target_pose.pose.position.y = 0.0
-    goal.target_pose.pose.orientation.w = 1.0
-    move_base.send_goal(goal)
-    move_base.wait_for_result()
-
+        i = np.random.randint(len(x_vect))
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = 'map'
+        goal.target_pose.pose.position.x = x_vect[i]
+        goal.target_pose.pose.position.y = y_vect[i]
+        orientation = quaternion_from_euler(0, 0, 3.14/2)
+        goal.target_pose.pose.orientation.x=orientation[0]
+        goal.target_pose.pose.orientation.y=orientation[1]
+        goal.target_pose.pose.orientation.z=orientation[2]
+        goal.target_pose.pose.orientation.w=orientation[3]
+        move_base.send_goal(goal)
+        move_base.wait_for_result()
+        r.sleep()
